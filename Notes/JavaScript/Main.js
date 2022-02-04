785ea7d7da0af9676eaf559d01e809eb23f649f2
Fabio Mignone <mignofax@gmail.com>
2022-02-04 12:16:05 +0100

    add settings
    need to add the change collor option in setting and introduction when page is loaded

--- Notes/JavaScript/Main.js
+++ Notes/JavaScript/Main.js
@@ -197,3 +197,20 @@ class App {
 
 const root = document.getElementById("app");
 const app = new App(root);
+
+function Settings() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
