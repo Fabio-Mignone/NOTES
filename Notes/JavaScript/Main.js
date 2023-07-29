223f83df2848bcba561250dc6f1282811cc92751
Fabio Mignone <mignofax@gmail.com>
2023-07-29 17:51:39 +0200

    Image added and translation
    image uploaded and translation done to the code

--- Notes/JavaScript/Main.js
+++ Notes/JavaScript/Main.js
@@ -1,206 +1,217 @@
 class App {
-  constructor(root) {
-      this.notes = [];
-      this.activeNote = null;
-      this.view = new NotesView(root, this._handlers());
+    constructor(root) {
+        this.notes = [];
+        this.activeNote = null;
+        this.view = new NotesView(root, this._handlers());
 
-      this._refreshNotes();
-  }
+        this._refreshNotes();
+    }
 
-  _refreshNotes() {
-      const notes = NotesAPI.getAllNotes();
+    _refreshNotes() {
+        const notes = NotesAPI.getAllNotes();
 
-      this._setNotes(notes);
+        this._setNotes(notes);
 
-      if (notes.length > 0) {
-          this._setActiveNote(notes[0]);
-      }
-  }
+        if (notes.length > 0) {
+            this._setActiveNote(notes[0]);
+        }
+    }
 
-  _setNotes(notes) {
-      this.notes = notes;
-      this.view.updateNoteList(notes);
-      this.view.updateNotePreviewVisibility(notes.length > 0);
-  }
+    _setNotes(notes) {
+        this.notes = notes;
+        this.view.updateNoteList(notes);
+        this.view.updateNotePreviewVisibility(notes.length > 0);
+    }
 
-  _setActiveNote(note) {
-      this.activeNote = note;
-      this.view.updateActiveNote(note);
-  }
+    _setActiveNote(note) {
+        this.activeNote = note;
+        this.view.updateActiveNote(note);
+    }
 
-  _handlers() {
-      return {
-          onNoteSelect: noteId => {
-              const selectedNote = this.notes.find(note => note.id == noteId);
-              this._setActiveNote(selectedNote);
-          },
-          onNoteAdd: () => {
-              const newNote = {
-                  title: "Nuova Nota",
-                  body: "Testo Nota..."
-              };
+    _handlers() {
+        return {
+            onNoteSelect: (noteId) => {
+                const selectedNote = this.notes.find((note) => note.id == noteId);
+                this._setActiveNote(selectedNote);
+            },
+            onNoteAdd: () => {
+                const newNote = {
+                    title: "New Note",
+                    body: "Note Text...",
+                };
 
-              NotesAPI.saveNote(newNote);
-              this._refreshNotes();
-          },
-          onNoteEdit: (title, body) => {
-              NotesAPI.saveNote({
-                  id: this.activeNote.id,
-                  title,
-                  body
-              });
+                NotesAPI.saveNote(newNote);
+                this._refreshNotes();
+            },
+            onNoteEdit: (title, body) => {
+                NotesAPI.saveNote({
+                    id: this.activeNote.id,
+                    title,
+                    body,
+                });
 
-              this._refreshNotes();
-          },
-          onNoteDelete: noteId => {
-              NotesAPI.deleteNote(noteId);
-              this._refreshNotes();
-          },
-      };
-  }
+                this._refreshNotes();
+            },
+            onNoteDelete: (noteId) => {
+                NotesAPI.deleteNote(noteId);
+                this._refreshNotes();
+            },
+        };
+    }
 }
 
-  class NotesAPI {
-  static getAllNotes() {
-      const notes = JSON.parse(localStorage.getItem("notesapp-notes") || "[]");
+class NotesAPI {
+    static getAllNotes() {
+        const notes = JSON.parse(localStorage.getItem("notesapp-notes") || "[]");
 
-      return notes.sort((a, b) => {
-          return new Date(a.updated) > new Date(b.updated) ? -1 : 1;
-      });
-  }
+        return notes.sort((a, b) => {
+            return new Date(a.updated) > new Date(b.updated) ? -1 : 1;
+        });
+    }
 
-  static saveNote(noteToSave) {
-      const notes = NotesAPI.getAllNotes();
-      const existing = notes.find(note => note.id == noteToSave.id);
+    static saveNote(noteToSave) {
+        const notes = NotesAPI.getAllNotes();
+        const existing = notes.find((note) => note.id == noteToSave.id);
 
-      // Edit/Update
-      if (existing) {
-          existing.title = noteToSave.title;
-          existing.body = noteToSave.body;
-          existing.updated = new Date().toISOString();
-      } else {
-          noteToSave.id = Math.floor(Math.random() * 1000000);
-          noteToSave.updated = new Date().toISOString();
-          notes.push(noteToSave);
-      }
+        // Edit/Update
+        if (existing) {
+            existing.title = noteToSave.title;
+            existing.body = noteToSave.body;
+            existing.updated = new Date().toISOString();
+        } else {
+            noteToSave.id = Math.floor(Math.random() * 1000000);
+            noteToSave.updated = new Date().toISOString();
+            notes.push(noteToSave);
+        }
 
-      localStorage.setItem("notesapp-notes", JSON.stringify(notes));
-  }
+        localStorage.setItem("notesapp-notes", JSON.stringify(notes));
+    }
 
-  static deleteNote(id) {
-      const notes = NotesAPI.getAllNotes();
-      const newNotes = notes.filter(note => note.id != id);
+    static deleteNote(id) {
+        const notes = NotesAPI.getAllNotes();
+        const newNotes = notes.filter((note) => note.id != id);
 
-      localStorage.setItem("notesapp-notes", JSON.stringify(newNotes));
-  }
+        localStorage.setItem("notesapp-notes", JSON.stringify(newNotes));
+    }
 }
 
+class NotesView {
+    constructor(root, { onNoteSelect, onNoteAdd, onNoteEdit, onNoteDelete } = {}) {
+        this.root = root;
+        this.onNoteSelect = onNoteSelect;
+        this.onNoteAdd = onNoteAdd;
+        this.onNoteEdit = onNoteEdit;
+        this.onNoteDelete = onNoteDelete;
+        this.root.innerHTML = `
+        <div class="notes__sidebar">
+            <button class="notes__add" type="button">Add Note</button>
+            <div class="notes__list"></div>
+        </div>
+        <div class="notes__preview" id="area">
+            <input class="notes__title" type="text" id="title" placeholder="New Note...">
+            <textarea class="notes__body" id="body">Note Text...</textarea>
+        </div>
+    `;
 
-  class NotesView {
-  constructor(root, { onNoteSelect, onNoteAdd, onNoteEdit, onNoteDelete } = {}) {
-      this.root = root;
-      this.onNoteSelect = onNoteSelect;
-      this.onNoteAdd = onNoteAdd;
-      this.onNoteEdit = onNoteEdit;
-      this.onNoteDelete = onNoteDelete;
-      this.root.innerHTML = `
-          <div class="notes__sidebar">
-              <button class="notes__add" type="button">Aggiungi Nota</button>
-              <div class="notes__list"></div>
-          </div>
-          <div class="notes__preview" id="area">
-              <input class="notes__title" type="text" id="titolo" placeholder="Nuova Nota...">
-              <textarea class="notes__body" id="testo">Testo Nota...</textarea>
-          </div>
-      `;
+        const btnAddNote = this.root.querySelector(".notes__add");
+        const inpTitle = this.root.querySelector(".notes__title");
+        const inpBody = this.root.querySelector(".notes__body");
 
-      const btnAddNote = this.root.querySelector(".notes__add");
-      const inpTitle = this.root.querySelector(".notes__title");
-      const inpBody = this.root.querySelector(".notes__body");
+        btnAddNote.addEventListener("click", () => {
+            this.onNoteAdd();
+        });
 
-      btnAddNote.addEventListener("click", () => {
-          this.onNoteAdd();
-      });
+        [inpTitle, inpBody].forEach((inputField) => {
+            inputField.addEventListener("blur", () => {
+                const updatedTitle = inpTitle.value.trim();
+                const updatedBody = inpBody.value.trim();
 
-      [inpTitle, inpBody].forEach(inputField => {
-          inputField.addEventListener("blur", () => {
-              const updatedTitle = inpTitle.value.trim();
-              const updatedBody = inpBody.value.trim();
+                this.onNoteEdit(updatedTitle, updatedBody);
+            });
+        });
 
-              this.onNoteEdit(updatedTitle, updatedBody);
-          });
-      });
+        this.updateNotePreviewVisibility(false);
+    }
 
-      this.updateNotePreviewVisibility(false);
-  }
+    _createListItemHTML(id, title, body, updated) {
+        const MAX_BODY_LENGTH = 60;
 
-  _createListItemHTML(id, title, body, updated) {
-      const MAX_BODY_LENGTH = 60;
+        return `
+        <div class="notes__list-item" data-note-id="${id}">
+            <div class="notes__small-title">${title}</div>
+            <div class="notes__small-body">
+                ${body.substring(0, MAX_BODY_LENGTH)}
+                ${body.length > MAX_BODY_LENGTH ? "..." : ""}
+            </div>
+            <div class="notes__small-updated">
+                ${new Date(updated).toLocaleString(undefined, {
+            dateStyle: "full",
+            timeStyle: "short",
+        })}
+            </div>
+        </div>
+    `;
+    }
 
-      return `
-          <div class="notes__list-item" data-note-id="${id}">
-              <div class="notes__small-title">${title}</div>
-              <div class="notes__small-body">
-                  ${body.substring(0, MAX_BODY_LENGTH)}
-                  ${body.length > MAX_BODY_LENGTH ? "..." : ""}
-              </div>
-              <div class="notes__small-updated">
-                  ${updated.toLocaleString(undefined, { dateStyle: "full", timeStyle: "short" })}
-              </div>
-          </div>
-      `;
-  }
+    updateNoteList(notes) {
+        const notesListContainer = this.root.querySelector(".notes__list");
 
-  updateNoteList(notes) {
-      const notesListContainer = this.root.querySelector(".notes__list");
+        // Empty list
+        notesListContainer.innerHTML = "";
 
-      // Empty list
-      notesListContainer.innerHTML = "";
+        for (const note of notes) {
+            const html = this._createListItemHTML(
+                note.id,
+                note.title,
+                note.body,
+                note.updated
+            );
 
-      for (const note of notes) {
-          const html = this._createListItemHTML(note.id, note.title, note.body, new Date(note.updated));
+            notesListContainer.insertAdjacentHTML("beforeend", html);
+        }
 
-          notesListContainer.insertAdjacentHTML("beforeend", html);
-      }
+        // Add select/delete events for each list item
+        notesListContainer.querySelectorAll(".notes__list-item").forEach((noteListItem) => {
+            noteListItem.addEventListener("click", () => {
+                this.onNoteSelect(noteListItem.dataset.noteId);
+            });
 
-      // Add select/delete events for each list item
-      notesListContainer.querySelectorAll(".notes__list-item").forEach(noteListItem => {
-          noteListItem.addEventListener("click", () => {
-              this.onNoteSelect(noteListItem.dataset.noteId);
-          });
+            noteListItem.addEventListener("dblclick", () => {
+                const doDelete = confirm("Are you sure you want to delete this note?");
 
-          noteListItem.addEventListener("dblclick", () => {
-              const doDelete = confirm("Sei sicuro di voler eliminare questa nota ?");
+                if (doDelete) {
+                    this.onNoteDelete(noteListItem.dataset.noteId);
+                }
+            });
+        });
+    }
 
-              if (doDelete) {
-                  this.onNoteDelete(noteListItem.dataset.noteId);
-              }
-          });
-      });
-  }
+    updateActiveNote(note) {
+        this.root.querySelector(".notes__title").value = note.title;
+        this.root.querySelector(".notes__body").value = note.body;
 
-  updateActiveNote(note) {
-      this.root.querySelector(".notes__title").value = note.title;
-      this.root.querySelector(".notes__body").value = note.body;
+        this.root.querySelectorAll(".notes__list-item").forEach((noteListItem) => {
+            noteListItem.classList.remove("notes__list-item--selected");
+        });
 
-      this.root.querySelectorAll(".notes__list-item").forEach(noteListItem => {
-          noteListItem.classList.remove("notes__list-item--selected");
-      });
+        this.root
+            .querySelector(`.notes__list-item[data-note-id="${note.id}"]`)
+            .classList.add("notes__list-item--selected");
+    }
 
-      this.root.querySelector(`.notes__list-item[data-note-id="${note.id}"]`).classList.add("notes__list-item--selected");
-  }
-
-  updateNotePreviewVisibility(visible) {
-      this.root.querySelector(".notes__preview").style.visibility = visible ? "visible" : "hidden";
-  }
+    updateNotePreviewVisibility(visible) {
+        this.root.querySelector(".notes__preview").style.visibility = visible ? "visible" : "hidden";
+    }
 }
 
 const root = document.getElementById("app");
 const app = new App(root);
 
-function understand(){
+
+function understand() {
     document.getElementById("opacity").classList.remove("opacity");
-    var introduzione = document.getElementById("introduzione");
+    var introduzione = document.getElementById("introduction");
     introduzione.removeChild(box);
 }
+
 
